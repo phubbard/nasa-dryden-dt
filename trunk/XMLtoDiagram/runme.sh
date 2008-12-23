65be8aea002c8bcc399eef649493fdbeeba4ce06
hubbard@sdsc.edu <hubbard@sdsc.edu@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2008-12-23 21:07:39 +0000

    experimenting with dot, layouts, and svg

--- trunk/XMLtoDiagram/runme.sh
+++ trunk/XMLtoDiagram/runme.sh
@@ -3,6 +3,8 @@
 
 ./xmlToDot.py P3_startup.xml > test.dot
 
+dot -Nfontname="/System/Library/Fonts/Times.dfont" test.dot -o test.dotlayout
+
 dot -Tpng -Nfontname="/System/Library/Fonts/Times.dfont" test.dot -o test.png
 
 dot -Tsvg -Nfontname="/System/Library/Fonts/Times.dfont" test.dot -o test.svg
