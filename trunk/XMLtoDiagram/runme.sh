e1fe5ef1046ea86a35f99be513346f5b635c4668
hubbard@sdsc.edu <hubbard@sdsc.edu@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2008-12-23 00:31:39 +0000

    Need to write a makefile, but this at least documents the steps

---
+++ trunk/XMLtoDiagram/runme.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# pfh 12/22/08, script before I rewrite as a makefile
+
+./xmlToDot.py > test.dot
+
+dot -Tpng -Nfontname="/System/Library/Fonts/Times.dfont" test.dot -o test.png
+
+dot2gxl test.dot > test.gxl
+
