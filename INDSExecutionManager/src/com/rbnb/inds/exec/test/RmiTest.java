dd18e15882a728b54e92241e8add73eb5bd9350c
whf@creare.com <whf@creare.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2008-12-23 16:00:23 +0000

    Added RMI interface for runtime interrogation of system status.

---
+++ INDSExecutionManager/src/com/rbnb/inds/exec/test/RmiTest.java
@@ -0,0 +1,51 @@
+/*
+	RmiTest.java
+	
+	Tests RMI implementation.
+	
+	2008/12/19  WHF  Created.
+*/
+
+package com.rbnb.inds.exec.test;
+
+import com.rbnb.inds.exec.Remote;
+
+public class RmiTest 
+{
+	/**
+	  * Test routine using RMI.
+	  */
+	public static void main(String[] args) throws Exception
+	{
+//		System.setSecurityManager(new SecurityManager());
+		
+		// Connect using RMI:
+		java.rmi.registry.Registry reg 
+				= java.rmi.registry.LocateRegistry.getRegistry();
+				
+		String[] names = reg.list();
+		int index = 0;
+		
+		Remote rem = (Remote) reg.lookup(names[index]);
+		String[] commands = rem.getCommandList();
+		for (String cmd : commands) {
+			System.err.print(cmd);
+			if (rem.isComplete(cmd)) System.err.println("  X");
+			else System.err.println();
+		}
+		
+		// Lookup specific commands, if any:
+		for (int ii = 0; ii < args.length; ++ii) {
+			String cmd = args[ii];
+			
+			System.err.println("\n\n****  "+cmd+"  ****");
+			System.err.println("----  Output Stream  ----");
+			System.err.println(rem.getCommandOut(cmd));
+			System.err.println("\n----  Error Stream  ----");
+			System.err.println(rem.getCommandError(cmd));
+			
+			System.err.println("\n----  Configuration  ----");
+			System.err.println(rem.getConfiguration(cmd));
+		}
+	}
+}
