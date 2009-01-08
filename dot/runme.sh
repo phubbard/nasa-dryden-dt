a7b5d7d0429c4e9f384d237457c6c50fad56d3fa
hubbard@sdsc.edu <hubbard@sdsc.edu@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2009-01-08 00:09:45 +0000

    Time to hack on the dotfile to figure out how to get the layout we want, then add those changes to the python once ready.

---
+++ dot/runme.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# pfh 12/22/08, script before I rewrite as a makefile
+
+dot -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.dotlayout
+dot -Tsvg -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.svg
+
+
