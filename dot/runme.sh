ea7e99b9e64cbb720b187768052c4e4bf177c979
hubbard@sdsc.edu <hubbard@sdsc.edu@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2009-01-09 23:17:48 +0000

    More experimentation; rankdir LR fixed the graph shape problem.

--- dot/runme.sh
+++ dot/runme.sh
@@ -3,5 +3,5 @@
 
 dot -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.dotlayout
 dot -Tsvg -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.svg
-
+dot -Tpng -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.png
 
