74748afb98527c117176f0d40fa05f05a35abc42
hubbard@sdsc.edu <hubbard@sdsc.edu@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2009-01-13 17:51:21 +0000

    Added a config file for dot and its options, bugfixes to osSpec and also load config file from same.

--- XMLtoDiagram/cgi-bin/osSpec.py
+++ XMLtoDiagram/cgi-bin/osSpec.py
@@ -8,35 +8,45 @@
 import sys
 import os
 import logging
- 
+import ConfigParser
 
 class indsDot:
 
 	# Variables and defaults
 	dotCmd = 'dot'
 	dotParams = ''
+	configFile = 'defaults.cfg'
 	
 	def __init__(self):		
-		# Log everything, and send it to stderr.
-		logging.basicConfig(level=logging.DEBUG)
-
-		if os.name == 'posix':
-			logging.debug('OS is POSIX')
-			pass
-			
-		# uname only works on posix systems
-		longName = os.uname()
 		
-		logging.debug('uname is ' + longName[0])
-	
-		# OSX requires specifying where to find TimesRoman
-		if longName[0] == 'Darwin':
-			self.dotParams = '-Nfontname=/System/Library/Fonts/Times.dfont'
-			logging.debug('Font name param set for OSX')
-		else:
-			logging.debug('Other POSIX OS, no params set for dot')
-	
 		# Anything windows-specific
 		if os.name == 'nt':
 			logging.debug('OS is NT, no paramaters set')
-
+		if os.name == 'posix':
+			logging.debug('OS is POSIX')
+			
+			# uname only works on posix systems
+			longName = os.uname()
+		
+			logging.debug('uname is ' + longName[0])
+	
+			# OSX requires specifying where to find TimesRoman
+			if longName[0] == 'Darwin':
+				self.dotParams = '-Nfontname=/System/Library/Fonts/Times.dfont'
+				logging.debug('Font name param set for OSX')
+			else:
+				logging.debug('Other POSIX OS, no params set for dot')
+	
+		# Let config file override defaults
+		config = ConfigParser.ConfigParser()
+		fn = config.read(self.configFile)
+		if fn:
+			logging.debug('config file %s opened ok', self.configFile)
+			logging.debug('dot is: ' + config.get('dot', 'dotcmd'))
+			logging.debug('dotparams are: ' + config.get('dot', 'dotparams'))
+			
+			self.dotCmd = config.get('dot', 'dotcmd')
+			self.dotParams = config.get('dot', 'dotparams')
+		else:
+			logging.warn('Unable to open configuration file')
+		
