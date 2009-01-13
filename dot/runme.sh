c9be2140749406ec64fee5bb0e4cd4d4f4f96f6a
hubbard@sdsc.edu <hubbard@sdsc.edu@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2009-01-13 02:52:12 +0000

    Working on changing the graph layout as requested. Code edits to match.

--- dot/runme.sh
+++ dot/runme.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # pfh 12/22/08, script before I rewrite as a makefile
 
-dot -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.dotlayout
 dot -Tsvg -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.svg
 dot -Tpng -Nfontname="/System/Library/Fonts/Times.dfont" p3-third.dot -o p3-third.png
 
