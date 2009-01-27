5ed2cb99dc8630a51fdc3432181f1f749d8541e2
hubbard@sdsc.edu <hubbard@sdsc.edu@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2009-01-27 21:52:50 +0000

    Added Doxygen and documentation generated from it

--- trunk/INDS/Utility/RBNB/V3.1B3/apache-tomcat-5.5.12/webapps/ROOT/WEB-INF/cgi/httptest.py
+++ trunk/INDS/Utility/RBNB/V3.1B3/apache-tomcat-5.5.12/webapps/ROOT/WEB-INF/cgi/httptest.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
-# A simple Python test case from Paul Hubbard showing a problem with HTTP
+## A simple Python test case from Paul Hubbard showing a problem with HTTP
 # requests when run on Windows from tomcat/cgi
+#
+# @file httptest.py
+# @bug urlopen fails on win32/tomcat/cgi
+# @date 1/27/09
 
 import urllib2
 
