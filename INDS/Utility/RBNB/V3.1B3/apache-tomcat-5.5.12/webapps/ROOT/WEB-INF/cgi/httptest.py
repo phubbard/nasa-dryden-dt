611004dafa7a335ba018df933e3c45d96c8c55e3
john.wilson@erigo.com <john.wilson@erigo.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2009-01-27 20:21:12 +0000

    Tweak to the Python test script illustrating the problem of issuing HTTP requests on Windows from tomcat/cgi; the script runs from tomcat under Mac but not under tomcat from Windows.

--- INDS/Utility/RBNB/V3.1B3/apache-tomcat-5.5.12/webapps/ROOT/WEB-INF/cgi/httptest.py
+++ INDS/Utility/RBNB/V3.1B3/apache-tomcat-5.5.12/webapps/ROOT/WEB-INF/cgi/httptest.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
 
-" A simple Python test case from Paul Hubbard showing a problem with HTTP "
-" requests when run on Windows from tomcat/cgi                            "
+# A simple Python test case from Paul Hubbard showing a problem with HTTP
+# requests when run on Windows from tomcat/cgi
 
 import urllib2
 
 fp = urllib2.urlopen('http://127.0.0.1/')
-
+print "Content-type: text/html"
+print
 print fp.read()
+
