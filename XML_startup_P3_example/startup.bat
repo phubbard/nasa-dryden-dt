7d7337d3925738f9c3238c2a1d3163e5cad332eb
john.wilson@erigo.com <john.wilson@erigo.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2009-03-09 18:46:20 +0000

    A few file tweaks, mainly to support low-level Execution Manager testing using NOAA_P3 data.

--- XML_startup_P3_example/startup.bat
+++ XML_startup_P3_example/startup.bat
@@ -1,6 +1,6 @@
 
 REM INDS startup batch file for Windows
-REM NOTE: We assume that JAVA_HOME is defined in the environment
+REM NOTE: We assume that JAVA_HOME and CATALINA_OPTS are defined in the environment
 
 REM Start Tomcat
 pushd ..\RBNB\V3.2B1\apache-tomcat-5.5.12\bin
