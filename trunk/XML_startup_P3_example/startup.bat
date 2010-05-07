177fc35701b285d7cc58ef7f8d6a399468e04758
john.wilson@erigo.com <john.wilson@erigo.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2010-05-07 18:25:03 +0000

    Upgrade system to use RBNB V3.2B3, which includes Tomcat 6.0.18.

--- trunk/XML_startup_P3_example/startup.bat
+++ trunk/XML_startup_P3_example/startup.bat
@@ -3,7 +3,9 @@ REM INDS startup batch file for Windows
 REM NOTE: We assume that JAVA_HOME and CATALINA_OPTS are defined in the environment
 
 REM Start Tomcat
-pushd ..\RBNB\V3.2B1\apache-tomcat-5.5.12\bin
+REM JPW 05/07/2010: New RBNB installation
+REM pushd ..\RBNB\V3.2B1\apache-tomcat-5.5.12\bin
+pushd ..\RBNB\V3.2B3\apache-tomcat-6.0.18\bin
 START "Tomcat" /MIN JavaEnv.exe jre startup
 popd
 ..\INDS-Utility\Sleep.exe 10000
