95fd61ed98ebb4ff8f469ea632bdf40ddd9f3178
john.wilson@erigo.com <john.wilson@erigo.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2010-06-30 12:58:07 +0000

    Tweak to the Linux shutdown script

--- trunk/XML_startup_P3_example/iemShutdown.sh
+++ trunk/XML_startup_P3_example/iemShutdown.sh
@@ -2,8 +2,11 @@
 echo "killing XMLDemux..."
 ps axww | grep java | grep XMLDemux | awk '{print "kill " $1}' | sh
 sleep 2
+echo "killing CSVDemux..."
+ps axww | grep java | grep CSVDemux | awk '{print "kill " $1}' | sh
+sleep 2
 echo "killing rbnb server..."
-ps axww | grep java | grep 3333 | grep '\-p' | awk '{print "kill " $1}' | sh
+ps axww | grep java | grep 3333 | grep '\-X' | awk '{print "kill " $1}' | sh
 sleep 10
 echo "killing rbnb clients..."
 ps axww | grep java | awk '{print "kill " $1}' | sh
