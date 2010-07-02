bc5e85baf2d6cc8c16f7d66627e57800a6af1449
john.wilson@erigo.com <john.wilson@erigo.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2010-07-02 15:07:32 +0000

    New iemShutdown.sh for the P3 sample project

--- trunk/XML_startup_P3_example/iemShutdown.sh
+++ trunk/XML_startup_P3_example/iemShutdown.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
-echo "killing XMLDemux..."
-ps axww | grep java | grep XMLDemux | awk '{print "kill " $1}' | sh
-sleep 2
+echo "killing DirectIP Server..."
+ps axww | grep java | grep DirectIPServer | awk '{print "kill " $1}' | sh
+sleep 3
 echo "killing CSVDemux..."
 ps axww | grep java | grep CSVDemux | awk '{print "kill " $1}' | sh
-sleep 2
+sleep 3
 echo "killing rbnb server..."
 ps axww | grep java | grep 3333 | grep '\-X' | awk '{print "kill " $1}' | sh
 sleep 10
-echo "killing rbnb clients..."
-ps axww | grep java | awk '{print "kill " $1}' | sh
-ps axww | grep java | grep FlightMonitor | awk '{print "kill " $1}' | sh
-sleep 10
-ps axww | grep java | grep 3333 | awk '{print "kill -9 " $1}' | sh
-sleep 1
-echo "killing tomcat..."
-ps axww | grep java | grep apache | awk '{print "kill " $1}' | sh
-sleep 10
-# Make sure Tomcat died
-ps axww | grep java | grep apache | awk '{print "kill -9 " $1}' | sh
+#echo "killing rbnb clients..."
+#ps axww | grep java | awk '{print "kill " $1}' | sh
+#sleep 10
+#ps axww | grep java | grep 3333 | awk '{print "kill -9 " $1}' | sh
+
