6638fa56649bd743619224b1c74b4fff404b7c73
john.wilson@erigo.com <john.wilson@erigo.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2010-07-09 12:51:25 +0000

    Updates to the UWyoKingAir project

--- UWyoKingAir/iemShutdown.sh
+++ UWyoKingAir/iemShutdown.sh
@@ -4,10 +4,14 @@ ps axww | grep java | grep DirectIPServer | awk '{print "kill " $1}' | sh
 sleep 3
 echo "killing CSVDemux..."
 ps axww | grep java | grep CSVDemux | awk '{print "kill " $1}' | sh
+echo "killing XMLDemux..."
+ps axww | grep java | grep XMLDemux | awk '{print "kill " $1}' | sh
 sleep 3
 echo "killing rbnb server..."
 ps axww | grep java | grep 3333 | grep '\-X' | awk '{print "kill " $1}' | sh
 sleep 10
+echo "killing Tomcat server..."
+ps axww | grep java | grep RBNB | awk '{print "kill " $1}' | sh
 #echo "killing rbnb clients..."
 #ps axww | grep java | awk '{print "kill " $1}' | sh
 #sleep 10
