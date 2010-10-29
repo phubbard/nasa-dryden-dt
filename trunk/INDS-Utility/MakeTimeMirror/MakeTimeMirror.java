7bfeeb68bcf16943ae58032a921c3bc645f17202
john.wilson@erigo.com <john.wilson@erigo.com@88d02224-ba5d-11dd-aa87-4b319d2fb4de>
2010-10-29 21:00:29 +0000

    Update MakeTimeMirror to use append mode (no longer match the upstream source's cache/archive/mode settings).

--- trunk/INDS-Utility/MakeTimeMirror/MakeTimeMirror.java
+++ trunk/INDS-Utility/MakeTimeMirror/MakeTimeMirror.java
@@ -8,12 +8,12 @@ import com.rbnb.sapi.Control;
  * MakeTimeMirror
  *
  * Create a time-based PUSH mirror, starting NOW and going on forever.  The
- * output/downstream source's cache and archive settings will be set to match
- * those settings of the original/upstream source.
+ * output/downstream source's is set as follows: cache = 1, archive = 10000,
+ * archive mode = append.
  *
  * Before making the mirror, we check to make sure we can connect to the
  * downstream source.  We will continue to try to connect to the downstream
- * source in a sleepy loop for up to 10 minutes.
+ * source in a sleepy loop for up to 15 minutes.
  *
  * If the upstream source's frames each contain multiple points (let's say
  * N points per frame) then the effective size of the downstream source is not
@@ -31,6 +31,7 @@ import com.rbnb.sapi.Control;
  * Modification History
  * --------------------
  * 10/28/2010  JPW  Created.
+ * 10/29/2010  MJM  Change to "append" archive mode; don't match upstream source.
  *
  */
 
@@ -103,10 +104,10 @@ public class MakeTimeMirror {
 	    toSourceName,
 	    com.rbnb.api.Mirror.NOW,
 	    com.rbnb.api.Mirror.CONTINUOUS,
-	    100,
-	    0,
-	    com.rbnb.api.SourceInterface.ACCESS_NONE,
-	    true,
+	    1,
+	    10000,
+	    com.rbnb.api.SourceInterface.ACCESS_APPEND,
+	    false,  // DON'T MATCH SOURCE; we want to force append mode
 	    0.);
 	
 	controller.stop();
